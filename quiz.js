06a5cf38c08cf10d4919ca1da85b599cb06b0f09
Prem <joshichaturbhuj2@gmail.com>
2025-10-02 18:09:23 -0400

    updated

--- quiz.js
+++ quiz.js
@@ -25,38 +25,40 @@ const data = [
     opt: ["Beijing", "Chengdu", "Shanghai", "Chonginq"],
   },
 ];
+
 const timerDiv = document.querySelector(".timer");
 const questionDiv = document.querySelector(".question");
 const optionsDiv = document.querySelector(".options");
 const options = document.querySelectorAll(".option");
-const scoreSpan = document.querySelector("#score span");
+const scoreSpan = document.querySelector(".score");
 const button = document.querySelector(".button");
 
 let count = 5;
 let questionNumber = 0;
+let score = 0;
 
-printQuestionAndOptions(); //PRINTS FIRST QUESTION & OPTIONS
+printQuestionAndOptions();
 
 let time = setInterval(() => {
   if (count === 1) {
-    if (questionNumber === data.length) {
-      //question will empty
+    if (questionNumber == data.length - 1) {
       clearInterval(time);
       timerDiv.style.display = "none";
       questionDiv.style.display = "none";
       optionsDiv.style.display = "none";
+      button.style.display = "none";
+      alert("Quiz finished! Your Score: " + score);
+    } else {
+      count = 5;
+      timerDiv.textContent = 5;
+      questionNumber++;
+      printQuestionAndOptions();
     }
-    count = 5;
-    timerDiv.textContent = 5;
-    // NEXT QUESTION
-    questionNumber++;
-    printQuestionAndOptions();
   } else {
     count--;
     timerDiv.textContent = count;
   }
 }, 1000);
-// clearInterval(time)
 
 function printQuestionAndOptions() {
   questionDiv.innerText = data[questionNumber].q;
@@ -64,40 +66,44 @@ function printQuestionAndOptions() {
     (option, index) => (option.innerText = data[questionNumber].opt[index])
   );
 
-  //   for (let i = 0; i < options.length; i++) {
-  //     options[i].innerText = data[questionNumber].opt[i];
-  //   }
-
   options.forEach((e) => {
     e.style.backgroundColor = "";
     e.style.pointerEvents = "auto";
 
-    e.addEventListener("click", () => {
-      options.forEach((e) => {
-        e.style.pointerEvents = "none";
+    e.onclick = () => {
+      options.forEach((o) => {
+        o.style.pointerEvents = "none";
       });
 
       if (e.innerText === data[questionNumber].a) {
         e.style.backgroundColor = "green";
+        score++;
+        console.log(score);
+        scoreSpan.innerText = score;
       } else {
         e.style.backgroundColor = "red";
-
         options.forEach((o) => {
           if (o.innerText === data[questionNumber].a) {
             o.style.backgroundColor = "green";
           }
         });
       }
-    });
+    };
   });
 }
 
 button.addEventListener("click", () => {
-  timerDiv.textContent = 5;
-  count = 5;
-  questionNumber++;
-  printQuestionAndOptions(); //PRINTS FIRST QUESTION & OPTIONS
+  if (questionNumber == data.length - 1) {
+    clearInterval(time);
+    timerDiv.style.display = "none";
+    questionDiv.style.display = "none";
+    optionsDiv.style.display = "none";
+    button.style.display = "none";
+    alert("Quiz finished! Your Score: " + score);
+  } else {
+    timerDiv.textContent = 5;
+    count = 5;
+    questionNumber++;
+    printQuestionAndOptions();
+  }
 });
-// function resercounter() {
-
-// }
